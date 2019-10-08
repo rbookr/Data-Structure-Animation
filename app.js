a6a13ab4691bc5e8d61d640565eea9af336ce2d5
rainboy <rainboylvx@qq.com>
2019-10-08 11:57:47 +0800

    [修改] 抛弃ui,使用整个app

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-$(window).bind('hashchange',function(){
-  alert('hashchange')
-})
+/*
+ *$(window).bind('hashchange',function(){
+ *  alert('hashchange')
+ *})
+ */
 
-//alert(get_url_hash())
-//
 class APP {
   constructor(){
     /* 初始化 */
     this.address = window.location.pathname
-
     this.http = new HTTP(this.address,'project/'+this.get_url_hash()+'/')
   }
 
@@ -16,21 +15,57 @@ class APP {
     return (window.location.hash.length > 0) ?
       window.location.hash.substring(1):""
   }
-
-  
 }
 
+let template = `
+<div class="app">
+  <div class="playground">
+    <div id="graph" style="text-align: center;"></div>
+  </div>
+
+<div class="ui">
+  <div class="ui-panel">
+  <div class="contorl">
+    <ul>
+      <li><button class="ui-button auto-play">自动播放</button></li>
+      <li><button class="ui-button pre-frame">上一帧</button></li>
+      <li><button class="ui-button next-frame">下一帧</button></li>
+      <li><button class="ui-button next-frame">停止</button></li>
+    </ul>
+  </div>
+    <div class="process">
+      <div class="process-info">
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+`
 /* 渲染组件 */
-new Vue({el:'#ui'})
+new Vue({
+  el:'#app',
+  template,
+	data:function(){
+	  return {
+	    app:{},
+	    player:{},
+	    name:"Data Structure Animation App by Rainboy",
+	  }
+	},
+	async mounted(){
+	  console.log('hello ap')
+	  var self = this
+    self.app = new APP()
 
-var app = new APP()
+    this.player =  await self.app.http.get('dump.json').then( (res)=>{
+      var player = new Player()
+      player.load(res)
+      return player
+    })
 
-app.http.get('dump.json').then( (res)=>{
-  var player = new Player()
-  player.load(res)
-  console.log(player)
-  console.log(player.get(0))
-  player.init( function(){
-    player.auto_player()
-  })
+    this.player.init( function(){
+      self.player.auto_player()
+    })
+	}
 })
+
